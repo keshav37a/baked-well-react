fbabc16c57e83d436c34807b00f220220d1476b4
Keshav Mathur <keshav.mathur@byjus.com>
2023-02-10 08:42:30 +0530

    add price; add poppins; add cart icon

--- src/components/BakeryItems/index.js
+++ src/components/BakeryItems/index.js
@@ -7,9 +7,9 @@ import styles from "./BakeryItems.module.css";
 function BakeryItems({ className, items }) {
   return (
     <div className={cx(styles["bakery-items"], className)}>
-      {items.map(({ name, coverImg, description, id }) => (
+      {items.map(({ name, coverImg, description, price, id }) => (
         <div key={id} class={styles["single-bakery-item"]}>
-          <div class={styles["bakery-item_cover_photo_container"]}>
+          <div className={styles["bakery-item_cover_photo_container"]}>
             <img
               alt="bakery-item"
               className={styles["bakery-item_cover_photo_img"]}
@@ -22,13 +22,18 @@ function BakeryItems({ className, items }) {
             {name}
           </h4>
           <p className={styles["bakery-item_desc"]}>{description}</p>
+          <h5
+            className={cx(styles["bakery-item_price"], styles["text-center"])}
+          >
+            ₹{price}
+          </h5>
           <div className={cx(styles["bakery-item-btn-container"])}>
             <Button
               className={cx(styles["bakery-item-btn-invisible"])}
               btnClassName={styles["bakery-item-btn"]}
               isFullWidth={true}
             >
-              Quick Add
+              Add to cart
             </Button>
           </div>
         </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,27 @@
-import "./index.css";
+import cartIcon from "../../assets/cart.ico";
+import styles from "./Header.module.css";
 
 function Header({
   showOrderBanner = true,
   orderBannerText = "Orders placed after 8pm will be delivered the next day",
 }) {
   return (
-    <div className="">
+    <div className={styles["header"]}>
       {showOrderBanner ? (
-        <div className="header-order-banner">
-          <p className="header-order-banner_msg">{orderBannerText}</p>
+        <div className={styles["header-order-banner"]}>
+          <p className={styles["header-order-banner_msg"]}>{orderBannerText}</p>
         </div>
       ) : null}
-      <div>Header</div>
+      <div className={styles["header-wrapper"]}>
+        <h1 className={styles["header-title"]}>Baked Well</h1>
+        <div className={styles["header-cart_wrapper"]}>
+          <img
+            alt="cart"
+            className={styles["header-cart_img"]}
+            src={cartIcon}
+          />
+        </div>
+      </div>
     </div>
   );
 }
